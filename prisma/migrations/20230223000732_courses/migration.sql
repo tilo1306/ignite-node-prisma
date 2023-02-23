bbb2bce2089b3278b0a263e5aebdbed320d42c18
Douglas <doug1306@gmail.com>
2023-02-22 21:08:34 -0300

    feat: create courses

--- prisma/migrations/20230223000732_courses/migration.sql
+++ prisma/migrations/20230223000732_courses/migration.sql
@@ -19,8 +19,24 @@ CREATE TABLE "modules" (
     CONSTRAINT "modules_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "courses_modules" (
+    "id" TEXT NOT NULL,
+    "fk_id_course" TEXT NOT NULL,
+    "fk_id_module" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "courses_modules_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "courses_name_key" ON "courses"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "modules_name_key" ON "modules"("name");
+
+-- AddForeignKey
+ALTER TABLE "courses_modules" ADD CONSTRAINT "courses_modules_fk_id_course_fkey" FOREIGN KEY ("fk_id_course") REFERENCES "courses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "courses_modules" ADD CONSTRAINT "courses_modules_fk_id_module_fkey" FOREIGN KEY ("fk_id_module") REFERENCES "modules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
